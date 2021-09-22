874a942fb88e0c5c6fb419839ffed8b704dd8b83
Francis Ian Aquino <37167282+francisianaquino@users.noreply.github.com>
2021-09-22 15:41:00 +0800

    :bug: Fixed anchors

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -53,9 +53,9 @@ export default function Contact() {
                             <h2 className="title-font font-semibold text-white tracking-widest text-xs">
                                 EMAIL
                             </h2>
-                            <a className="text-indigo-400 leading-relaxed">
-                                reedbarger@email.com
-                            </a>
+                            <button className="text-indigo-400 leading-relaxed">
+                                fisaquino@gmail.com
+                            </button>
                             <h2 className="title-font font-semibold text-white tracking-widest text-xs mt-4">
                                 PHONE
                             </h2>
